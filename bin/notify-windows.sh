cb84310b1e4d164a2fa657bd464e980f57bcca74
netakiryosuke <sigoryosuke@gmail.com>
2026-04-04 15:27:29 +0900

    Fix PowerShell injection in notify-windows.sh

--- bin/notify-windows.sh
+++ bin/notify-windows.sh
@@ -2,15 +2,17 @@
 # Sends a toast notification on Windows via WSL using PowerShell.
 # Usage: notify-windows.sh <title> [message]
 
-TITLE="$1"
-MESSAGE="${2:-}"
+NOTIFY_TITLE="$1"
+NOTIFY_MESSAGE="${2:-}"
 
+# Pass values via environment variables to avoid PowerShell code injection.
+NOTIFY_TITLE="$NOTIFY_TITLE" NOTIFY_MESSAGE="$NOTIFY_MESSAGE" \
 powershell.exe -Command "
   Add-Type -AssemblyName System.Windows.Forms
   \$n = New-Object System.Windows.Forms.NotifyIcon
   \$n.Icon = [System.Drawing.SystemIcons]::Information
   \$n.Visible = \$true
-  \$n.ShowBalloonTip(5000, '$TITLE', '$MESSAGE', [System.Windows.Forms.ToolTipIcon]::Info)
+  \$n.ShowBalloonTip(5000, \$env:NOTIFY_TITLE, \$env:NOTIFY_MESSAGE, [System.Windows.Forms.ToolTipIcon]::Info)
   Start-Sleep -Seconds 6
   \$n.Dispose()
 " 2>/dev/null &
